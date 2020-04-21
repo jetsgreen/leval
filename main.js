288c5f169933b999b56557775d7605dc674e8a7e
Felix Fernandez <fernandezfelix460.gmail.com>
2020-04-21 14:39:00 -0400

    playing aroung with responsivenss #2

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 toggleButton = document.getElementsByClassName('toggle-button')[0];
 navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
+
 toggleButton.addEventListener('click', () =>{
     navbarLinks.classList.toggle('active')
 } )
+
+navbarLinks.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
